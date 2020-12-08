f84385f3a3ad2e008ec2e5e140d71b63e596b9d8
李卓原 <zhuoyuan@cece.com>
2020-12-08 15:34:16 +0800

    将推送相关设置单独出来

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.max,
-);
+import 'package:flutter/material.dart';
+import 'package:flutter_google_fcm/notification_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -36,84 +26,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-
   RemoteMessage message;
+  String token;
 
   @override
   void initState() {
-    initFirebase();
-    initChannel();
     super.initState();
-  }
-
-  initFirebase() async {
-    String token = await messaging.getToken();
-    print('Token:$token');
-
-    ///ios , mac, web需要请求权限
-    NotificationSettings settings = await messaging.requestPermission(
-      alert: true,
-      announcement: false,
-      badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
-      sound: true,
-    );
-    print('User granted permission: ${settings.authorizationStatus}');
-
-    ///ios启用前台通知
-    await messaging.setForegroundNotificationPresentationOptions(
-      alert: true, // Required to display a heads up notification
-      badge: true,
-      sound: true,
-    );
-
-    ///前台消息
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('在前台收到消息！');
-      setState(() {
-        this.message = message;
-      });
-
-      if (message.notification != null) {
-        RemoteNotification notification = message.notification;
-
-        ///显示通知
-        if (notification != null && notification.android != null) {
-          FlutterLocalNotificationsPlugin().show(
-              notification.hashCode,
-              notification.title,
-              notification.body,
-              NotificationDetails(
-                android: AndroidNotificationDetails(
-                  channel.id,
-                  channel.name,
-                  channel.description,
-                  // TODO add a proper drawable resource to android, for now using
-                  //      one that already exists in example app.
-                  icon: 'launch_background',
-                ),
-              ));
-        }
-      }
-    });
-
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('从后台打开！');
-    });
-
-    var m = await messaging.getInitialMessage();
-    if (m != null) {
-      print('应用从终止状态打开:${m?.notification?.title}');
-      setState(() {
-        message = m;
-      });
-    }
-
+    initFirebase();
+ 
   }
 
   @override
@@ -123,7 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         child: Column(
           children: [
-            Text('fcm'),
+            Text('fcm token:$token'),
             Text('title:${message?.notification?.title},\nbody:${message?.notification?.body},'),
           ],
         ),
@@ -131,16 +51,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void initChannel() async {
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-
-    await flutterLocalNotificationsPlugin
-        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-  }
-
-  static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-    print("收到后台消息: ${message.messageId}");
+  void initFirebase() async {
+    await NotificationManager.init();
+    token = await NotificationManager.getToken();
+    setState(() {});
   }
 }
--- lib/notification_manager.dart
+++ lib/notification_manager.dart
@@ -1,4 +1,101 @@
-///FCM配置
-class NotificationManager{
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+///FCM配置
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.max,
+);
+
+class NotificationManager {
+  static Future<String> getToken() async {
+    String token = await FirebaseMessaging.instance.getToken();
+    print('Token:$token');
+    return token;
+  }
+
+  static init() async {
+    await Firebase.initializeApp();
+
+    var initializationSettingsAndroid = new AndroidInitializationSettings('launch_background');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+
+    FlutterLocalNotificationsPlugin().initialize(
+        InitializationSettings(
+            android: initializationSettingsAndroid, iOS: initializationSettingsIOS),
+        onSelectNotification: _onSelectNotification);
+
+    ///ios , mac, web需要请求权限
+    NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    ///ios启用前台通知
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, // Required to display a heads up notification
+      badge: true,
+      sound: true,
+    );
+
+    ///前台消息
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('在前台收到消息！');
+
+      if (message.notification != null) {
+        RemoteNotification notification = message.notification;
+
+        ///显示通知
+        if (notification != null && notification.android != null) {
+          FlutterLocalNotificationsPlugin().show(
+              notification.hashCode,
+              notification.title,
+              notification.body,
+              NotificationDetails(
+                android: AndroidNotificationDetails(
+                  channel.id,
+                  channel.name,
+                  channel.description,
+                  // TODO add a proper drawable resource to android, for now using
+                  //      one that already exists in example app.
+                  icon: 'launch_background',
+                ),
+              ),
+              payload: message.data.toString());
+        }
+      }
+    });
+
+    ///后台消息
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    ///点击后台消息打开App
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('从后台打开！');
+    });
+
+    ///应用从终止状态打开
+    var m = await FirebaseMessaging.instance.getInitialMessage();
+    if (m != null) {
+      print('应用从终止状态打开:${m?.notification?.title}');
+    }
+  }
+
+  static Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    print("收到后台消息: ${message.messageId}");
+  }
+
+  static Future _onSelectNotification(String payload) {
+    print('前台通知点击:$payload');
+  }
 }
